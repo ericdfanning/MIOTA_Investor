22735638d9398811bc1d93a783a65f9e656281d0
Eric F <ericdfanning@gmail.com>
2018-02-24 19:29:32 -0600

    updated email.config gitignore instructions

--- email.config.js
+++ email.config.js
@@ -1,6 +1,8 @@
 module.exports.serviceEmailAddress = 'EMAIL_YOU_WANT_IT_SENT_FROM'
 module.exports.password = 'PASSWORD_TO_EMAIL_ABOVE'
 
+// *** NOTE: Once you update your private info ^^^, add this file to .gitignore. ".gitignore"
+
 // example
 // module.exports.email = 'emailname@gmail.com'
 // module.exports.password = 'mypassword'
